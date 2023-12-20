ea4a9b86000fb0d8783890c6f535cb048d57db9f
Zahir Alam <30320094+mesepith@users.noreply.github.com>
2023-12-20 12:52:05 +0000

    nsdl loop 25 days

--- fii/fpi-sectore-invest-nsdl-trial.py
+++ fii/fpi-sectore-invest-nsdl-trial.py
@@ -1,43 +1,28 @@
-from datetime import date
-
-today = date.today()
-
-crawl_date = today.strftime("%B%d%Y")
-print("crawl_date =", crawl_date)
-
+from datetime import date, timedelta
 import requests
 import pandas as pd
 
-#import fii_functionz
+# Calculate the start date (25 days ago)
+start_date = date.today() - timedelta(days=25)
 
-#url = 'https://www.fpi.nsdl.co.in/web/StaticReports/Fortnightly_Sector_wise_FII_Investment_Data/FIIInvestSector_'+crawl_date+'.html'
-url = 'https://www.fpi.nsdl.co.in/web/StaticReports/Fortnightly_Sector_wise_FII_Investment_Data/FIIInvestSector_Nov302023.html'
+# Iterate from start_date to today
+for n in range(26):
+    crawl_date = (start_date + timedelta(n)).strftime("%b%d%Y")
+    url = 'https://www.fpi.nsdl.co.in/web/StaticReports/Fortnightly_Sector_wise_FII_Investment_Data/FIIInvestSector_'+crawl_date+'.html'
+    print(url)
 
-print(url)
-#exit()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    html = requests.get(url, headers=headers, timeout=30).content
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-html = requests.get(url, headers=headers, timeout=30).content
+    try:
+        df_list = pd.read_html(html)
+        tablez = df_list[0]
+        json_data = tablez.to_json(orient = "split")
+        print(json_data)
 
-df_list = pd.read_html(html)
-
-count=0
-        
-tablez = df_list[count]
-
-json_data = tablez.to_json(orient = "split")
-
-print(json_data)
-
-date = today.strftime("%b-%d-%Y")
-
-#fii_functionz.postNsdlSectoreInvestDataofFpi(date, json_data)  
-
-API_URL = "https://staging-trade.zahiralam.com/fii-dii/get-nsdl-sectore-invest-data-of-fpi-fii";
-#print(API_URL); exit();
-data = {'json_data':json_data, 
-            'date':date,
-            'server':'hostinger',} 
-    
-# sending post request and saving response as response object : 
-r = requests.post(url = API_URL, data = data)
+        date_str = (start_date + timedelta(n)).strftime("%b-%d-%Y")
+        API_URL = "https://staging-trade.zahiralam.com/fii-dii/get-nsdl-sectore-invest-data-of-fpi-fii"
+        data = {'json_data':json_data, 'date':date_str, 'server':'hostinger'} 
+        r = requests.post(url = API_URL, data = data)
+    except ValueError:
+        print(f"No table found for {url}")
