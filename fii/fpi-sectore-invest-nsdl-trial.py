a7ec80289f4494675663c552b1a4d934f870deb0
mesepith <zahiralam29@gmail.com>
2024-07-07 14:33:33 +0530

    month naming change

--- fii/fpi-sectore-invest-nsdl-trial.py
+++ fii/fpi-sectore-invest-nsdl-trial.py
@@ -7,9 +7,10 @@ start_date = date.today() - timedelta(days=25)
 
 # Iterate from start_date to today
 for n in range(26):
-    crawl_date = (start_date + timedelta(n)).strftime("%b%d%Y")
+    crawl_date = (start_date + timedelta(n)).strftime("%B%d%Y")
     url = 'https://www.fpi.nsdl.co.in/web/StaticReports/Fortnightly_Sector_wise_FII_Investment_Data/FIIInvestSector_'+crawl_date+'.html'
     print(url)
+    # exit();
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
     html = requests.get(url, headers=headers, timeout=30).content
